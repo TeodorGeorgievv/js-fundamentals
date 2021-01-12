59f799a0311585bc175e7fd6d3fe70402a4b3b7f
tedsyy <tedsyy@gmail.com>
2021-01-12 02:36:37 +0200

    Lab 1
    Basic Syntax, Conditional Statements and Loops

---
+++ MultiplyNumBy2.js
@@ -0,0 +1,6 @@
+function multiplyNum(num) {
+    let result = num * 2;
+    console.log(result); 
+}
+
+multiplyNum(2)
---
+++ NumsFromMtoN.js
@@ -0,0 +1,7 @@
+function numsMToN(num1, num2) {
+    for (let i = num1; i >= num2; i--) {
+        console.log(i);
+    }
+}
+
+numsMToN(6, 2)
